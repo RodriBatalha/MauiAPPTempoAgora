dcbe6d7b07ca9be3410f4d89de9e92cac768d17b
RodriBatalha <Rodrigobatalha001@gmail.com>
2025-04-06 23:45:32 -0300

    Add files via upload

---
+++ Services/DataService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MauiAppTempoAgora.Models;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "2449c701e16a7057bc57ade1ccc04b75";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JsonObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+
+
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString()
+                    };//fecha obj do Tempo.
+
+                }//fecha if se o status do servidor for de sucesso
+            }// fecha laço using
+
+            return t;
+        }
+    }
+}
